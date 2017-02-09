7629baee6200c03e0996941b984f6c57a86e5d86
John Evan Blight <jeblight@gmail.com>
2017-02-09 09:20:49 +0200

    Add project files.

---
+++ IOStreamProgram/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IOStreamProgram
+{
+    //Structure practice
+    /**Creating stuct that creates a words array
+     * Requests words as input and populates the
+     * array and then displays the words in order*/
+    struct Words
+    {
+        public string getWords(string[] str)
+        {
+            string words_combined = "Your words were: " + str[0] + str[1] + str[2] + str[3] + ".";
+
+            return words_combined;
+        }
+    };
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Text that displays on first run
+            String txt = "Hi there, please 4 words...";
+            Console.WriteLine("{0}", txt);
+
+            //Write words to string[]
+            //Loops 4 times and requests input from user
+            string[] str = new string[5];
+
+            for (int i = 0; i < 4; i++)
+            {
+                int c = i + 1;
+                Console.WriteLine("Word {0}:", c);
+                str[i] = Console.ReadLine();
+            }
+
+            Words w = new Words();
+            Console.WriteLine(w.getWords(str));
+
+            try
+            {
+                //Creating instance of StreamWriter
+                //Writes data from string[] to file temp.txt
+                //using statement also closes StreamWiter
+                using (StreamWriter sw = new StreamWriter("temp.txt"))
+                {
+                    foreach (string s in str)
+                    {
+                        sw.WriteLine(s);
+                    }
+                }
+
+                //Creating an instance of StreamReader
+                //Using statement also closes the StreamReader
+                using (StreamReader sr = new StreamReader("temp.txt"))
+                {
+                    string line;
+
+                    //Read file until all line are read
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("There was an error reading the file");
+                Console.WriteLine("Exception: {0}", e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
