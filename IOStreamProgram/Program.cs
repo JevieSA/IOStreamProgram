514e8ccce3e556faface8f2c352bd7068a1a433e
John Evan Blight <jeblight@gmail.com>
2017-02-09 10:41:35 +0200

    Added Checks for if users want to add more words

--- IOStreamProgram/Program.cs
+++ IOStreamProgram/Program.cs
@@ -29,54 +29,62 @@ namespace IOStreamProgram
             String txt = "Hi there, please 4 words...";
             Console.WriteLine("{0}", txt);
 
-            //Write words to string[]
-            //Loops 4 times and requests input from user
-            string[] str = new string[5];
+            string more = "n";
 
-            for (int i = 0; i < 4; i++)
+            do
             {
-                int c = i + 1;
-                Console.WriteLine("Word {0}:", c);
-                str[i] = Console.ReadLine();
-            }
+                //Write words to string[]
+                //Loops 4 times and requests input from user
+                string[] str = new string[5];
 
-            Words w = new Words();
-            Console.WriteLine(w.getWords(str));
-
-            try
-            {
-                //Creating instance of StreamWriter
-                //Writes data from string[] to file temp.txt
-                //using statement also closes StreamWiter
-                using (StreamWriter sw = new StreamWriter("temp.txt", true))
+                for (int i = 0; i < 4; i++)
                 {
-                    foreach (string s in str)
-                    {
-                        sw.WriteLine(s);
-                    }
+                    int c = i + 1;
+                    Console.WriteLine("Word {0}:", c);
+                    str[i] = Console.ReadLine();
                 }
 
-                //Creating an instance of StreamReader
-                //Using statement also closes the StreamReader
-                using (StreamReader sr = new StreamReader("temp.txt"))
+                Words w = new Words();
+                Console.WriteLine(w.getWords(str));
+
+                try
                 {
-                    string line;
-
-                    //Read file until all line are read
-                    while ((line = sr.ReadLine()) != null)
+                    //Creating instance of StreamWriter
+                    //Writes data from string[] to file temp.txt
+                    //using statement also closes StreamWiter
+                    using (StreamWriter sw = new StreamWriter("temp.txt", true))
                     {
-                        Console.WriteLine(line);
+                        foreach (string s in str)
+                        {
+                            sw.WriteLine(s);
+                        }
                     }
+
+                    //Creating an instance of StreamReader
+                    //Using statement also closes the StreamReader
+                    using (StreamReader sr = new StreamReader("temp.txt"))
+                    {
+                        string line;
+
+                        //Read file until all line are read
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            Console.WriteLine(line);
+                        }
+                    }
+
                 }
+                catch (Exception e)
+                {
+                    Console.WriteLine("There was an error reading the file");
+                    Console.WriteLine("Exception: {0}", e.Message);
+                }
+                Console.WriteLine("Would you like to add more words?");
+                more = Console.ReadLine();
 
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("There was an error reading the file");
-                Console.WriteLine("Exception: {0}", e.Message);
-            }
+            } while (more.Equals("y", StringComparison.OrdinalIgnoreCase));
 
-            Console.ReadLine();
-        }
+                    Console.ReadLine();
+            }
     }
-}
+    }
